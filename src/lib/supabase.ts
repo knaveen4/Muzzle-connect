b8ab5e1d8b6bcade616c0885a3fe1a740afa5927
KOTHAKAPU NAVEEN REDDY <112322972+knaveen4@users.noreply.github.com>
2025-08-05 12:41:58 +0530

    Update supabase.ts
    SUpabase keys are hiden

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 import { SupabaseClient } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'  // 👈 Important for URL issues in RN
 
-const supabaseUrl = 'https://yrogunuhonkdhesvcrno.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlyb2d1bnVob25rZGhlc3Zjcm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0MTQ3NjQsImV4cCI6MjA2ODk5MDc2NH0.xq6YBfpmymkksGJI-7KNIrEhtZYQK9HlprwJkS4df34';
+const supabaseUrl = 'ASK'
+const supabaseAnonKey = 'ASK'
 
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
